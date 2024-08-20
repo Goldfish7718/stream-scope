1451c35f37746e2645b2a0097f1332ec7ced98c2
Tejas <tejasnanoti2@gmail.com>
2024-08-20 23:15:09 +0530

    fix: Fix object import in utils;

--- utils/index.ts
+++ utils/index.ts
@@ -1,11 +1,9 @@
 import analyzeSentiment from "./analyzeSentiment";
 import extractVideoId from "./extractId";
 import generateSummary from "./generateSummary";
-import { sentimentObjectCopy } from "./analyzeSentiment";
 
 export {
     analyzeSentiment,
     extractVideoId,
     generateSummary,
-    sentimentObjectCopy
 }
